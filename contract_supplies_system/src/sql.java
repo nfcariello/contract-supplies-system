4c450e0e28c845bcfa7433d53c769359a9337e28
nfcariello <ncariell@nyit.edu>
2019-04-22 23:40:55 -0400

    Started building queries

--- contract_supplies_system/src/sql.java
+++ contract_supplies_system/src/sql.java
@@ -1,18 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class sql {
 
 
-    /**
-     * Connect to the test.db.sqlite.sqlite database
-     *
-     * @return the Connection object
-     */
     private Connection connect() {
-        // SQLite connection string
         String url = "jdbc:sqlite:contract_supplies_system/src/db.sqlite";
         Connection conn = null;
         try {
@@ -23,13 +14,20 @@ public class sql {
         return conn;
     }
 
-    /**
-     * Insert a new row into the warehouses table
-     *
-     * @param name
-     * @param address
-     */
-    public void insert(String name, String address) {
+    public void insert_contracts(int supplier_number, Date contract_date) {
+        String sql = "INSERT INTO CONTRACTS(`SUPPLIER-NO`,`DATE-OF-CONTRACT`) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, supplier_number);
+            pstmt.setDate(2, contract_date);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insert_suppliers(String name, String address) {
         String sql = "INSERT INTO Suppliers(`SUPPLIER-NAME`,`SUPPLIER-ADDRESS`) VALUES(?,?)";
 
         try (Connection conn = this.connect();
@@ -40,56 +38,6 @@ public class sql {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-
-    public void start_connection1() {
-        Connection conn = null;
-        try {
-            // db.sqlite.sqlite parameters
-            String url = "jdbc:sqlite:db.sqlite.sqlite";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-
-    }
-
-    public void query() {
-        String query = "SELECT * FROM CONTRACTS WHERE CONTRACT-NO = 1";
-
-    }
-
-
-    public void start_connection() {
-        Connection c = null;
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:db.sqlite.sqlite");
-            System.out.println(c);
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        System.out.println("Opened database successfully");
-    }
-
-
-    public void sql_query_string() {
-
     }
 
 }
